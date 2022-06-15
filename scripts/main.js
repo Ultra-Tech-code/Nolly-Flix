56a01055c5362dd83f59221af88549fbc22d3efc
Adewale IyanuOluwa Isaac <60206492+Ultra-Tech-code@users.noreply.github.com>
2022-06-15 04:37:14 +0100

    pushed project guideline

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,29 @@
- const tabItems=document.querySelectorAll('.tab-item');
- const tabContent=document.querySelectorAll('.tab-content-item');
-//select tab content item
-function selectItem(e){
-    removeItem();
-    removeShow();
-    //add border to current tab
-    this.classList.add('tab-border');
-    //grab content items from dom
-    const tabContentItem=document.querySelector(`#${this.id}-content`);
-    console.log(tabContentItem);
-    tabContentItem.classList.add('show');
-}
-function removeItem(){
-    tabItems.forEach(item=>{
-        item.classList.remove('tab-border');
-    });
-}
-function removeShow(){
-    tabContent.forEach(item=>{
-        item.classList.remove('show');
-    });
-}
- tabItems.forEach(item=>{
-     item.addEventListener('click',selectItem)
- });
+//  const tabItems=document.querySelectorAll('.tab-item');
+//  const tabContent=document.querySelectorAll('.tab-content-item');
+// //select tab content item
+// function selectItem(e){
+//     removeItem();
+//     removeShow();
+//     //add border to current tab
+//     this.classList.add('tab-border');
+//     //grab content items from dom
+//     const tabContentItem=document.querySelector(`#${this.id}-content`);
+//     console.log(tabContentItem);
+//     tabContentItem.classList.add('show');
+// }
+// function removeItem(){
+//     tabItems.forEach(item=>{
+//         item.classList.remove('tab-border');
+//     });
+// }
+// function removeShow(){
+//     tabContent.forEach(item=>{
+//         item.classList.remove('show');
+//     });
+// }
+//  tabItems.forEach(item=>{
+//      item.addEventListener('click',selectItem)
+//  });
 
 
 //  ------------------------------------------------------------
@@ -32,15 +32,46 @@ function removeShow(){
 //     Bgimage.style.backgroundimage =   url('../images/tab-content-2-3.png');
 
 // }
+{/* <button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" style="width:100px"></img> */}
 
 // var theBtn = document.querySelectorAll("btn");
 // theBtn.onclick = changeBg;
 
-counter = 0;
-var no0fCount = document.querySelector('.counter');
-no0fCount.innerHTML = counter;
+
+const btnItems=document.querySelectorAll('.btn');
+// let result = document.querySelector('.counter')
+var counter = 0;
+
+// Array.from(btnItems).forEach(btn =>{
+//     btn.addEventListener('click', function(event){
+//         result.innerHTML = counter++;
+//     });
+// });
+
+function selectItem(e){
+    e.preventDefault();
+    increment()
+
+    // var noOfCount = document.querySelector(`#counter-${this.id}`);
+    // console.log(noOfCount)
+    // noOfCount.innerHTML = counter;
+    const element = e.target;
+    if (element.tagName == "BUTTON"){
+        var result = e.target.getAttribute('.btn-lg');
+        result.innerHTML = counter;
+    }
+    // console.log(element)
+    // element.innerText = counter;
+    
+}
+
 
 function increment(){
-    counter++;
-    no0fCount.innerHTML = counter;
+    return counter++;
 }
+
+ btnItems.forEach(item=>{
+     item.addEventListener('click',selectItem)
+ });
